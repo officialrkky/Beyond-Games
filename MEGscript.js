e4b45213eb7b46413a668ba37929b7f7912d994c
officialrkky <93412800+officialrkky@users.noreply.github.com>
2022-03-01 13:59:25 +0800

    Update MEGscript.js

--- MEGscript.js
+++ MEGscript.js
@@ -1,95 +1,72 @@
-function getQuestionInput() {
-    var questionInput = document.getElementById("questionInput").value; 
-    alert(input);
+let randomNumber = Math.floor((Math.random() * 20) + 1);
+let output;
+
+if (randomNumber == 1){
+ let output = "It is decidedly so.";
+}
+else if (randomNumber == 2){
+  let output = "As i see it, yes.";
+}
+else if (randomNumber == 3){
+    let output = "Reply hazy, try again.";
+}
+else if (randomNumber == 4){
+    let output = "Don't count on it.";    
+}
+else if (randomNumber == 5){
+    let output = "It is decidedly so.";
+}
+else if (randomNumber == 6){
+    let output = "Most likely";
+}
+else if (randomNumber == 7){
+    let output = "Ask again later.";
+}
+else if (randomNumber == 8){
+    let output = "My reply is no";
+}
+else if (randomNumber == 9){
+    let output = "Without a doubt";
+}
+else if (randomNumber == 10){
+  let output = "Outlook good.";
+}
+else if (randomNumber == 11){
+    let output = "Better not tell you now.";
+}
+else if (randomNumber == 12){
+    let output = "My sources say no.";   
+}
+else if (randomNumber == 13){
+    let output = "Yes definitely.";
+}
+else if (randomNumber == 14){
+    let output = "Yes.";
+}
+else if (randomNumber == 15){
+    let output = "Cannot predict now.";
+}
+else if (randomNumber == 16){
+    let output = "Outlook not so good.";
+}
+else if (randomNumber == 17){
+    let output = "You may rely on it.";
+}
+else if (randomNumber == 18){
+    let output = "Signs point to yes.";
+}
+else if (randomNumber == 19){
+    let output = "Concentrate and ask again.";
+}
+else if (randomNumber == 20){
+    let output = "Very doubtful.";
+}
+else {
+    let output = "Error, please try again.";
 }
 
+document.getElementById("questionOutput").innerHTML = output;
 
-var randomNumber = Math.floor(Math.random() * 20);
-var outputNumber = "";
 
-switch (randomNumber) {
-  
-  case 1:
-    outputNumber += "It is certain.";
-    break;
-    
-  case 2:
-    outputNumber += "As i see it, yes.";
-    break;
-    
-  case 3:
-    outputNumber += "Reply hazy, try again.";
-    break;
-    
-  case 4:
-    outputNumber += "Don't count on it.";
-    break;
-    
-  case 5:
-    outputNumber += "It is decidedly so.";
-    break;
-    
-  case 6:
-    outputNumber += "Most likely.";
-    break;
-    
-  case 7:
-    outputNumber += "Ask again later.";
-    break;
-    
-  case 8:
-    outputNumber += "My reply is no.";
-    break;
-    
-  case 9:
-    outputNumber += "Without a doubt.";
-    break;
-    
-  case 10:
-    outputNumber += "Outlook good.";
-    break;
-    
-  case 11:
-    outputNumber += "Better not tell you now.";
-    break;
-    
-  case 12:
-    outputNumber += "My sources say no.";
-    break;  
-    
-  case 13;
-    outputNumber += "Yes definitely.";
-    break;  
-  
-  case 14:
-    outputNumber += "Yes.";
-    break; 
-  
-  case 15:
-    outputNumber += "Cannot predict now.";
-    break;  
-  
-  case 16:
-    outputNumber += "Outlook not so good.";
-    break;  
-  
-  case 17:
-    outputNumber += "You may rely on it.";
-    break;  
-  
-  case 18:
-    outputNumber += "Signs point to yes.";
-    break;  
-  
-  case 19:
-    outputNumber += "Concentrate and ask again.";
-    break;  
-  
-  case 20:
-    outputNumber += "Very doubtful.";
-    break;  
-    
-  default:
-    errorMessage += "Error, please try again"
-    
-}
+
+   
